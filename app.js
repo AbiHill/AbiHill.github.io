849dfbb9837d33dcc707626556f5a7158fc50039
AbiHill <abihill8@gmail.com>
2018-04-23 20:51:54 +0100

    hero fixed and amended plus removed video

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ function myFunction() {
     x.className = 'topnav';
   }
 }
+
+// THE BELOW FUNCTIONS DO AUTO SCROLL ON THE PAGE
+// function pageScroll() {
+//   window.scrollBy(0,1);
+//   let scrolldelay = setTimeout(pageScroll,10);
+// }
+//
+//
+// pageScroll();
+
+// var x = 1; //y-axis pixel displacement
+// var y = 1; //delay in milliseconds
+//
+// setInterval(function() {
+//   window.scroll(0, x);
+//   x = x + 0.2; //if you want to increase speed simply increase increment interval
+// }, y);
