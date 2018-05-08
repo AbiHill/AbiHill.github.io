879d45f09be5dd6a44302dd77a2423dc03b3b136
AbiHill <abihill8@gmail.com>
2018-05-08 22:52:37 +0100

    fixed flexbox on box four and three

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log('JS running');
 
+//amend the below so the nav bar works on click
 function myFunction() {
   var x = document.getElementById('myTopnav');
   if (x.className === 'topnav') {
@@ -8,3 +9,17 @@ function myFunction() {
     x.className = 'topnav';
   }
 }
+
+// change this to vanilla javascript - function to smooth the top nav link clicks
+$(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    const $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+});
