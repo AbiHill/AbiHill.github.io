2939521fee4aef1c35c6ee861f0958ce2d038eef
AbiHill <abihill8@gmail.com>
2018-05-09 10:21:40 +0100

    amended the javascript to jquery

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-console.log('JS running');
+$( document ).ready(function() {
+  console.log('JS Working');
 
-//amend the below so the nav bar works on click
-function myFunction() {
-  var x = document.getElementById('myTopnav');
-  if (x.className === 'topnav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'topnav';
+  //Dom Elements
+  const $x = $('.icon');
+
+
+
+
+  //mobile x click
+  $x.on('click', topNavToggle);
+
+  function topNavToggle() {
+    console.log('topnav clicked');
+    const $topnav = $('#myTopnav');
+    if ($topnav.className === ' topnav') {
+      $topnav.className += ' responsive';
+    } else {
+      $topnav.className = 'topnav';
+    }
   }
-}
 
-// change this to vanilla javascript - function to smooth the top nav link clicks
-$(document).ready(function(){
-  $('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
-    var target = this.hash;
-    const $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-    }, 900, 'swing', function () {
-      window.location.hash = target;
-    });
-  });
+
+
+
+
+
+
+
+
+//   // change this to vanilla javascript - function to smooth the top nav link clicks
+//   $(document).ready(function(){
+//     $('a[href^="#"]').on('click',function (e) {
+//       e.preventDefault();
+//       var target = this.hash;
+//       const $target = $(target);
+//       $('html, body').stop().animate({
+//         'scrollTop': $target.offset().top
+//       }, 900, 'swing', function () {
+//         window.location.hash = target;
+//       });
+//     });
+//   });
+//
 });
