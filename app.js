4da35592793a2f99cda992dbc19437f42c8e614a
AbiHill <abihill8@gmail.com>
2018-05-10 11:25:14 +0100

    jquery function for top nav smooth scroll and mobile nav

--- app.js
+++ app.js
@@ -11,35 +11,51 @@ $( document ).ready(function() {
   $x.on('click', topNavToggle);
 
   function topNavToggle() {
-    console.log('topnav clicked');
     const $topnav = $('#myTopnav');
-    if ($topnav.className === ' topnav') {
-      $topnav.className += ' responsive';
+    if (!$topnav.hasClass('responsive')) {
+      $topnav.addClass('responsive');
     } else {
-      $topnav.className = 'topnav';
+      $topnav.removeClass('responsive');
     }
   }
 
+  // function to smooth the top nav link clicks
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname === this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
 
 
-
-
-
-
-
-
-//   // change this to vanilla javascript - function to smooth the top nav link clicks
-//   $(document).ready(function(){
-//     $('a[href^="#"]').on('click',function (e) {
-//       e.preventDefault();
-//       var target = this.hash;
-//       const $target = $(target);
-//       $('html, body').stop().animate({
-//         'scrollTop': $target.offset().top
-//       }, 900, 'swing', function () {
-//         window.location.hash = target;
-//       });
-//     });
-//   });
-//
 });
