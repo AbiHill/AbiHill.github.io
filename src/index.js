1fecd8027ce9b5fdd3ae12f379428a6509bf7e7a
AbiHill <abihill8@gmail.com>
2020-04-14 20:12:40 +0100

    included webpack and my own version of a html template - need to fix image errors and missing images etc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import _ from 'lodash';
 import './style.scss';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
-  
+import './app.js';
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Abi Hill | Web Developer London',
+                template: './index.html'
+            }),
+        ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
     module: {
         rules: [
@@ -30,6 +46,10 @@ module.exports = {
                 use: [
                     'file-loader',
                 ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
             },
         ],
     },
